946ba28fcb756a58a52310f1eb9ca03d9a151162
arjun singh <arjunvkas@gmail.com>
2026-01-21 15:34:22 +0530

    [Arjun] Add. Some LeetCode Function

---
+++ datastructure-csharp-practice/leet-code-codebase/MergeSort.cs
@@ -0,0 +1,33 @@
+public class Solution
+{
+    public void Merge(int[] nums1, int m, int[] nums2, int n)
+    {
+        int i = m - 1;        // pointer for nums1
+        int j = n - 1;        // pointer for nums2
+        int k = m + n - 1;    // pointer for final position
+
+        // Merge from the end
+        while (i >= 0 && j >= 0)
+        {
+            if (nums1[i] > nums2[j])
+            {
+                nums1[k] = nums1[i];
+                i--;
+            }
+            else
+            {
+                nums1[k] = nums2[j];
+                j--;
+            }
+            k--;
+        }
+
+        // Copy remaining nums2 elements (if any)
+        while (j >= 0)
+        {
+            nums1[k] = nums2[j];
+            j--;
+            k--;
+        }
+    }
+}
