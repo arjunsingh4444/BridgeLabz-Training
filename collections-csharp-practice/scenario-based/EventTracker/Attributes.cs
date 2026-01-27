1fcb11240647b7f271169661eda002067923ff1f
arjun singh <arjunvkas@gmail.com>
2026-01-27 16:16:21 +0530

    [Arjun] Add. Some Scenario Functions

---
+++ collections-csharp-practice/scenario-based/EventTracker/Attributes.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+[AttributeUsage(AttributeTargets.Method)]
+public class AuditTrailAttribute : Attribute
+{
+    public string Description { get; }
+    public AuditTrailAttribute(string description = "")
+    {
+        Description = description;
+    }
+}
---
+++ collections-csharp-practice/scenario-based/EventTracker/IScannerI.cs
@@ -0,0 +1,7 @@
+﻿using System.Collections.Generic;
+
+public interface IScannerI
+{
+    List<EventLog> ScanClasses();
+    void GenerateJSON(string filePath);
+}
