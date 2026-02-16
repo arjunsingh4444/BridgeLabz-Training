c39c7b846c3700673ba11edcc6da6e4ea497f12e
arjun singh <arjunvkas@gmail.com>
2026-02-16 16:29:39 +0530

    [Arjun] Add. Address Book System

---
+++ ioprogramming-csharp-practice/scenario-based/AddressBookSystem/IAddressBook.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace BridgeLabzTraining.oops_csharp_practice.scenario_based.address_book
+{
+    // Interface defining all operations supported by an Address Book
+    internal interface IAddressBook
+    {
+        void AddContact();
+        void EditContact();
+        void DeleteContact();
+        void AddMultipleContactsMenu();
+        void SearchByCity(string city);
+        void SearchByState(string state);
+        int GetCountByCity(string city);
+        int GetCountByState(string state);
+        void SortContactsByName();
+        void SortContactsByCity();
+        void SortContactsByState();
+        void SortContactsByZip();
+        void DisplayAllContacts();
+        void AddToCityStateDictionary(
+            Dictionary<string, List<string>> cityMap,
+            Dictionary<string, List<string>> stateMap
+        );
+    }
+}
